9c4339c7cb09294a1b01b901db39b92edf15b8a5
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-06-06 22:05:03 -0300

    :tada: Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import GlobalStyles from './styles/global';
 import Header from './components/Header';
 import { CartProvider } from './hooks/useCart';
 
-const App = (): JSX.Element => {
+const App = (): JSX.Element => { 
   return (
     <BrowserRouter>
       <CartProvider>
